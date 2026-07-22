a80e57ff390f5d012677d98086bb7f2c6f0cfe7d
nitto05 <satyammohapatra2005@gmail.com>
2026-07-22 13:06:18 +0530

    1st commit, c++ simple arithmetic

---
+++ C++ PROGS/test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main()
+{
+    int age;
+    float height;
+    std::cout<< "enter age and height : \n";
+    std :: cin >> age ;
+    std :: cin >> height;
+    std :: cout << "the age and height of the kid is : " << age << " " << height;
+    // printf("hello world");
+    return  0;
+}
